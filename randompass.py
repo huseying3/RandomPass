99b31798a952c89b365cff03a4ef80914346f4e5
byh4ckerr <65884146+byh4ckerr@users.noreply.github.com>
2020-10-22 01:25:46 +0300

    Create randompass.py

---
+++ randompass.py
@@ -0,0 +1,384 @@
+import time
+
+import random
+
+import string
+
+print("=="*20)
+
+print("|                                        ")
+
+print("| En Güçlü Şifre Oluşturucu")
+
+print("|------Coded By H4cker-----")
+
+print("|                                         ")
+
+print("=="*20)
+
+time.sleep(2)
+
+print("")
+
+print("Sadece 4 ile 20 haneli bir şifre yapabilirsin")
+
+print("")
+
+time.sleep(2)
+
+a1=input("Şifre Uzunluğu:>>")
+
+a=["1","2","3","4","5","6","7","8","9"]
+
+A=random.choice(a)
+
+A1=random.choice(a)
+
+A2=random.choice(a)
+
+A3=random.choice(a)
+
+b=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+B=random.choice(b)
+
+B1=random.choice(b)
+
+B2=random.choice(b)
+
+B3=random.choice(b)
+
+c=string.punctuation
+
+C=random.choice(c)
+
+C1=random.choice(c)
+
+C2=random.choice(c)
+
+C3=random.choice(c)
+
+d=string.ascii_uppercase
+
+D=random.choice(d)
+
+D1=random.choice(d)
+
+D2=random.choice(d)
+
+D3=random.choice(d)
+
+if a1=="3":
+
+  print("")
+
+  print("....Hata....")
+
+  
+
+if a1=="2":
+
+  print("")
+
+  print("....Hata....")
+
+  
+
+if a1=="1":
+
+  print("")
+
+  print("....Hata....")
+
+  
+
+if a1=="4":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*6)
+
+  print("")
+
+  print(A,C1,B3,D2)
+
+  print("____________")
+
+  
+
+if a1=="5":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(A1,D3,C,B2,D)
+
+  print("__"*17)
+
+  
+
+  
+
+if a1=="6":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(D,C1,B1,D,A3,D3)
+
+  print("__"*17)
+
+  
+
+if a1=="7":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(C2,D2,B3,A1,D,C3,B)
+
+  print("__"*17)
+
+  
+
+if a1=="8":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(B,A2,D,D1,C2,B2,A1,D3)
+
+  print("__"*17)
+
+  
+
+if a1=="10":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(A2,B3,C2,B1,A3,A,D2,D,D3,B2)
+
+  print("__"*17)
+
+  
+
+  
+
+if a1=="9":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(A,A2,A3,D,C2,B1,B2,A2,D3)
+
+  print("__"*17)
+
+  
+
+  
+
+if a1=="11":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(A2,A,D1,D3,B1,B,B3,C1,C,A,A1)
+
+  print("__"*17)
+
+  
+
+if a1=="12":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(D,C1,B1,D,A3,C2,D2,B3,A1,D1,C3,B)
+
+  print("__"*17)
+
+  
+
+if a1=="14":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(A,C1,B3,D2,A2,B3,C2,B1,A3,A,D2,D,D3,B2)
+
+  print("__"*17)
+
+  
+
+if a1=="13":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(D,C1,B1,D3,D,A3,C2,D2,B3,A1,D1,C3,B)
+
+  print("__"*17)
+
+  
+
+if a1=="15":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(A1,D3,C,B2,D,A2,B3,C2,B1,A3,A,D2,D,D3,B2)
+
+  print("__"*17)
+
+  
+
+if a1=="16":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(B,A2,D,D1,C2,B2,A1,D3,A,C1,B3,D2,B3,C2,B1,D3)
+
+  print("__"*17)
+
+  
+
+if a1=="17":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(C2,D2,B3,A1,D,C3,B,A1,D3,C,B2,D,A2,B2,B1,C2,D)
+
+  print("__"*17)
+
+  
+
+if a1=="18":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(D,C1,B1,D,A3,C2,D2,B3,A1,D2,C3,B,A1,D3,C,B2,D,A)
+
+  print("__"*17)
+
+  
+
+  
+
+if a1=="19":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(D3,A2,B3,C2,B1,A3,A,D2,D,D3,B2,A,A3,D,C2,B1,B2,A2,D3)
+
+  print("__"*17)
+
+  
+
+if a1=="20":
+
+  print("")
+
+  print("Güçlü Şifren")
+
+  print("=="*17)
+
+  print("")
+
+  print(B,A2,D1,D,C2,B2,A1,D3,A,C1,B3,D2,B3,D2,C1,A,C2,B3,A1,D3)
+
+  print("__"*17)
+
+  
+
+ 
+
+if a1>"21":
+
+  print("")
+
+  print("Malesef...")
+
+  print("Şifren 4 ile 20 hane arasında olmalıdır")
+
